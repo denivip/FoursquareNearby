0297c3db886918d6ea9de7d4d1b2da415937b24a
Ilya Puchka <ilya@denivip.ru>
2012-11-23 11:35:07 +0400

    Initial Commit

---
+++ FoursquareNearby/DVAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  DVAppDelegate.h
+//  FoursquareNearby
+//
+//  Created by Ilya Puchka on 23.11.12.
+//  Copyright (c) 2012 Denivip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DVViewController;
+
+@interface DVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) DVViewController *viewController;
+
+@end
---
+++ FoursquareNearby/DVViewController.h
@@ -0,0 +1,13 @@
+//
+//  DVViewController.h
+//  FoursquareNearby
+//
+//  Created by Ilya Puchka on 23.11.12.
+//  Copyright (c) 2012 Denivip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DVViewController : UIViewController
+
+@end
